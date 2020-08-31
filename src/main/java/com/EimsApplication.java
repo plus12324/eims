28187a54589dfa0ef800d85efafa592e35d9692e
sungb <sungb@LAPTOP-MK7FN7NI>
2020-09-01 00:00:08 +0900

    init()

---
+++ src/main/java/com/EimsApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EimsApplication.class, args);
+	}
+
+}
